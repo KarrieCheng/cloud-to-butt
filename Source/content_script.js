1d4221e7ce335947428c7f29e046eb90fdaceb8b
Karrie Cheng <karriecheng@tamu.edu>
2017-07-14 08:53:18 -0400

    Lispify

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,24 +1,24 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
-	
+
 	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
 	    || node.classList.indexOf('ace_editor') > -1) {
 		return;
 	}
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -32,16 +32,31 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\\bS/g, "Th");
+	v = v.replace(/\\bCe/g, "Th");
+	v = v.replace(/\\bs/g, "th");
+	v = v.replace(/\\bss/g, "th");
+	v = v.replace(/\\bce/g, "th");
+	v = v.replace(/\\b!/g, getExcitedKarrieism());
+	v = v.replace(/\\b\.{3}/g, getSadKarrieism());
+	v = v.replace(/\\bDo not/g, "Donut");
+	v = v.replace(/\\bDo Not/g, "Donut");
+	v = v.replace(/\\bdo not/g, "DONUT");
+
 	textNode.nodeValue = v;
 }
 
 
+function getExcitedKarrieism(){
+    var sayings = ["... OH MAN!", "... YUP!", "...yEEEEE~", "! -power poses-"];
+    return sayings[Math.floor(Math.random() * sayings.length)];
+}
+
+function getSadKarrieism(){
+    var sayings = ["... :cry:", "... :(", "hnghhhh", "... yeee..."];
+    return sayings[Math.floor(Math.random() * sayings.length)];
+}
